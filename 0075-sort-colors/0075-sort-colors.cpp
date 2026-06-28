b850b575cdc0695893b32d1ef786f920ebba342f
Kunal Patel <kunal77professional@gmail.com>
2026-06-28 11:42:32 +0530

    Time: 0 ms (100%), Space: 11.7 MB (48.48%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int count0 = 0, count1 = 0, count2 = 0;
+        for (int num : nums) {
+            if (num == 0)
+                count0++;
+            else if (num == 1)
+                count1++;
+            else
+                count2++;
+        }
+        int index = 0;
+        while (count0--) {
+            nums[index++] = 0;
+        }
+        while (count1--) {
+            nums[index++] = 1;
+        }
+        while (count2--) {
+            nums[index++] = 2;
+        }
+    }
+};
